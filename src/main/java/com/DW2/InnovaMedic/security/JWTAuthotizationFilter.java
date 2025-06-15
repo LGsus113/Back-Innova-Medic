1bec73f47addedf3695b4383f61550e4ec3287cd
deividxdex <deivid2004acgmail.com>
2025-06-15 07:26:12 -0500

    Update

--- src/main/java/com/DW2/InnovaMedic/security/JWTAuthotizationFilter.java
+++ src/main/java/com/DW2/InnovaMedic/security/JWTAuthotizationFilter.java
@@ -6,12 +6,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.DW2.InnovaMedic.util.Token;
 
 import java.io.IOException;
 
+@Component
 public class JWTAuthotizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {

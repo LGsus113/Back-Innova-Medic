ea29ece6f081be264645f3e4f48e0cc92df1d880
LGsus113 <weikgoggowb@gmail.com>
2025-06-19 16:02:41 -0500

    Implementacion de horario en perfil de medicos

--- src/main/java/com/DW2/InnovaMedic/service/impl/MaintenanceUsuarioImpl.java
+++ src/main/java/com/DW2/InnovaMedic/service/impl/MaintenanceUsuarioImpl.java
@@ -1,10 +1,13 @@
 package com.DW2.InnovaMedic.service.impl;
 
+import com.DW2.InnovaMedic.dto.perfil.DisponibilidadMedicaDTO;
 import com.DW2.InnovaMedic.dto.perfil.MedicoPerfilDTO;
 import com.DW2.InnovaMedic.dto.perfil.PacientePerfilDTO;
+import com.DW2.InnovaMedic.entity.DisponibilidadMedica;
 import com.DW2.InnovaMedic.entity.Medico;
 import com.DW2.InnovaMedic.entity.Paciente;
 import com.DW2.InnovaMedic.entity.Usuario;
+import com.DW2.InnovaMedic.repository.DisponibilidadMedicaRepository;
 import com.DW2.InnovaMedic.repository.UsuarioRepository;
 import com.DW2.InnovaMedic.service.MaintenanceUsuario;
 import jakarta.transaction.Transactional;
@@ -12,11 +15,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class MaintenanceUsuarioImpl implements MaintenanceUsuario {
     @Autowired
     UsuarioRepository usuarioRepository;
+
+    @Autowired
+    DisponibilidadMedicaRepository disponibilidadMedicaRepository;
 
     @Override
     @Cacheable(value = "perfilUsuario", key = "#idUsuario")
@@ -25,12 +33,23 @@ public class MaintenanceUsuarioImpl implements MaintenanceUsuario {
                 .orElseThrow(() -> new Exception("Usuario no encontrado."));
 
         if (usuario instanceof Medico medico) {
+            List<DisponibilidadMedica> disponibilidades = disponibilidadMedicaRepository.findByMedico_IdUsuario(idUsuario);
+
+            List<DisponibilidadMedicaDTO> disponibilidadesDTO = disponibilidades.stream()
+                    .map(d -> new DisponibilidadMedicaDTO(
+                            d.getDiaSemana().name(),
+                            d.getHoraInicio(),
+                            d.getHoraFin()
+                    ))
+                    .toList();
+
             return new MedicoPerfilDTO(
                     medico.getSexo(),
                     medico.getTelefono(),
                     medico.getEmail(),
                     medico.getEspecialidad(),
-                    medico.getNumeroColegiado()
+                    medico.getNumeroColegiado(),
+                    disponibilidadesDTO
             );
         } else if (usuario instanceof Paciente paciente) {
             return new PacientePerfilDTO(
